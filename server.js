959141965fbbc813e18e1f407c141643e67665f1
Emma Amechu <eamechu@gmail.com>
2019-01-05 12:41:06 +0300

    Added ENV variable PROJECT_NAME

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = require('./app');
 
 
 const port = process.env.PORT || 3000;
+const projectName = process.env.PROJECT_NAME;
 
 const server = http.createServer(app);
 
 server.listen(port, ()=>{
+  console.log(projectName);
   console.log(`Server started on port: ${port}`);
 })
